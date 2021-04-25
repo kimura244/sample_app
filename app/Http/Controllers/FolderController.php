e6ac542fd2fc44e58b7a7d185fd9bbeae7ea5d2e
kimura244 <80373006+kimura244@users.noreply.github.com>
2021-04-25 19:13:47 +0900

    Initial commit

---
+++ app/Http/Controllers/FolderController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Folder; // ★ この行を追記！
+use App\Http\Requests\CreateFolder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class FolderController extends Controller
+{
+    public function showCreateForm()
+    {
+        return view('folders/create');
+    }
+
+    public function create(CreateFolder $request)
+    {
+        // フォルダモデルのインスタンスを作成する
+        $folder = new Folder();
+        // タイトルに入力値を代入する
+        $folder->title = $request->title;
+
+        // インスタンスの状態をデータベースに書き込む
+        Auth::user()->folders()->save($folder);
+
+        return redirect()->route('tasks.index', [
+            'folder' => $folder->id,
+        ]);
+    }
+}
